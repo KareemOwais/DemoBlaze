dc58c928e4c627c075a4f6f75db5fc9fa9e62969
KareemOwais <kareemmohamed70.km@gmail.com>
2025-04-09 21:25:11 +0200

    CSV_DataProvider

--- src/test/java/com/swaglabs/tests/Test1.java
+++ src/test/java/com/swaglabs/tests/Test1.java
@@ -1,41 +1,50 @@
+
 package com.swaglabs.tests;
 
-import Factory.WebDriverFactory;
+import DemoBlaze.Utils.CSVDataReader;
 import DemoBlaze.pages.CartPage;
 import DemoBlaze.pages.HomePage;
 import DemoBlaze.pages.ProductPage;
+import Factory.WebDriverFactory;
+import java.util.Iterator;
 import org.openqa.selenium.WebDriver;
-
-
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-
 public class Test1 {
+    WebDriver driver;
 
-    WebDriver driver ;
+    public Test1() {
+    }
 
-    @Test
-    public void test1() {
+    @DataProvider(
+            name = "loginData"
+    )
+    public Iterator<Object[]> getLoginData() {
+        return CSVDataReader.readCsv("test1.csv");
+    }
+
+    @Test(dataProvider = "loginData")
+    public void test1(String Username, String Password, String Catergory1, String Product1, String Catergory2,
+                      String Product2, String Name, String CreditCard, String Country, String City, String Year, String Month) {
         this.driver = WebDriverFactory.getDriver();
-        driver.get("https://demoblaze.com/");
-        driver.manage().window().maximize();
+        this.driver.get("https://demoblaze.com/");
+        this.driver.manage().window().maximize();
         HomePage homePage = new HomePage();
-        homePage.Login("kareem1234","1234");
         ProductPage productPage = new ProductPage();
-        homePage.ChooseCategory("phones").
-                ChooseProduct("Samsung galaxy s7");
+        CartPage cartPage = new CartPage();
+        homePage.Login(Username, Password);
+        homePage.ChooseCategory(Catergory1).ChooseProduct(Product1);
         productPage.clickAddToCart();
         homePage.NavigateTO("Home");
-        homePage.ChooseCategory("phones").
-                ChooseProduct("Samsung galaxy s7");
+        homePage.ChooseCategory(Catergory2).ChooseProduct(Product2);
         productPage.clickAddToCart();
         homePage.NavigateTO("cart");
-        CartPage cartPage = new CartPage();
-        cartPage.deleteProductFromCart("Samsung galaxy s7");
-        Assert.assertEquals(cartPage.ActualPrice(),cartPage.expectedPrice());
+        cartPage.deleteProductFromCart(Product2);
+        Assert.assertEquals(cartPage.ActualPrice(), cartPage.expectedPrice());
         cartPage.ClickPlaceOrderButton();
-        cartPage.fillOrderDetails("kareem","1234567890","Cairo","Egypt","123456","2024");
+        cartPage.fillOrderDetails(Name, Country, City, CreditCard, Month, Year);
         cartPage.clickPurchaseButton();
         cartPage.clickConfirmationButton();
         WebDriverFactory.closeDriver();
